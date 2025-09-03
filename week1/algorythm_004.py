92d09d4911dd5db3bd4f0832bbe488866f5bcacf
Marco Rossel <andresmarcorossel@gmail.com>
2025-09-03 00:30:10 -0400

    py algorithms reational numbers

---
+++ week1/algorythm_004.py
@@ -0,0 +1,14 @@
+# .44444 + infinity
+# algorithm to convert it into a fraction number
+# .44444 = x
+# 10x = 4.4444
+# 10x - x = 4
+# 9x = 4
+# x = 4/9
+
+# in the case of .99999 is equal to 1, look.
+# x = .99999
+# 10x = 9.9999
+# 10x - x = 9
+# 9x = 9
+# x = 9/9 = 1
