5ab6a36e38b1328f7117247e4c1a0a8874bd2aeb
Marco Rossel <andresmarcorossel@gmail.com>
2025-10-05 20:27:29 -0300

    backend path 5 - return of the think python book

--- think_python/unit_4/draw.py
+++ think_python/unit_4/draw.py
@@ -1,3 +1,19 @@
-from think_python.unit_4.jupyturtle import jupyturtle
+""" import jupyturtle
 
-print(jupyturtle)
+# print(display)
+jupyturtle.make_turtle()
+jupyturtle.forward(100)
+
+jupyturtle.show()
+# display(jupyturtle.get_turtle())
+from IPython.display import display
+ """
+
+import jupyturtle
+
+jupyturtle.make_turtle()
+jupyturtle.forward(100)
+
+# Guardar como archivo HTML
+with open('turtle_drawing.html', 'w') as f:
+    f.write(jupyturtle.get_SVG().data)
